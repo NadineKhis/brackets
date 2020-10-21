cb60a6ef42e0bcf9756137dd89658321c3b50b94
nadinekhis <torontino123@gmail.com>
2020-10-21 22:28:10 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let opening = bracketsConfig.map(pair => pair[0])
+    let closing = bracketsConfig.map(pair => pair[1])
+    let stack = []
+    for (let char of str) {
+        if (opening.includes(char) && (!closing.includes(char))) {
+            stack.push(char)
+            console.log(`pushed ${char}, stack is ${stack}`)
+        }
+
+        else if (opening.includes(char) && closing.includes(char)) {
+            if (stack.length === 0 || stack[stack.length - 1] !== char){
+                stack.push(char)
+                console.log(`pushed ${char}, stack is ${stack}`)
+                continue
+            }
+            let top = stack.pop()
+            console.log(`popped ${top}, char is ${char}`)
+            if (!(closing.indexOf(char) === opening.indexOf(top))){
+                console.log(`${char} !== ${top}`)
+                return false
+            }
+        }
+
+        else {
+            if (stack.length === 0)
+                return false
+            top = stack.pop()
+            console.log(`popped ${top}, char is ${char}`)
+            if (!(closing.indexOf(char) === opening.indexOf(top))) {
+                console.log(`${char} !== ${top}`)
+                return false
+            }
+        }
+    }
+
+    return stack.length === 0
+
 }
